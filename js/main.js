e4c6bbfcb10d875dce7fca7aed3f678d0d95808c
vu.dovan <vu.dovan@vietis.com.vn>
2019-01-22 17:50:16 +0700

    Update 22

---
+++ js/main.js
@@ -0,0 +1,92 @@
+var slideIndex = 1;
+// showSlide(slideIndex);
+
+function plusSlide(n) {
+  changeSlide(slideIndex += n);
+}
+
+function changeSlide(n) {
+  var leng = 3;
+  var arrow_next = document.getElementById("arrow_next");
+  var arrow_previous = document.getElementById("arrow_previous");
+  if (n > leng) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = leng;
+  }
+  showDots(slideIndex);
+  showArrow(slideIndex);
+  showSlide(slideIndex);
+}
+
+function showArrow(n) {
+  if (n == 1) {
+    arrow_previous.classList.add("slide__arrow--hidden");
+  } else {
+    arrow_previous.classList.remove("slide__arrow--hidden");
+  }
+  if (n == 3) {
+    arrow_next.classList.add("slide__arrow--hidden");
+  } else {
+    arrow_next.classList.remove("slide__arrow--hidden");
+  }
+}
+
+function showDots(n) {
+  var dots = document.getElementsByClassName("slide-dots__item");
+  for (var i = 0; i < dots.length; i++) {
+    if (n - 1 == i) {
+      dots[i].innerHTML = '<i class="fas fa-circle"></i>';
+    } else {
+      dots[i].innerHTML = '<i class="far fa-circle"></i>';
+    }
+  }
+}
+
+function showSlide(n) {
+  var slides = document.getElementsByClassName("slide-list__item");
+  for (var i=0; i < slides.length; i++) {
+    slides[i].classList.remove("slide-list__item--active");
+  }
+  slides[n-1].classList.add("slide-list__item--active");
+}
+
+var pageIndex = 1;
+
+function plusPage(n) {
+  changePage(pageIndex += n);
+}
+
+function changePage(n) {
+  var pages = document.getElementsByClassName("paging-number__item");
+
+  if (n < 1) {
+    pageIndex = pages.length;
+  } else {
+    if (n > pages.length) {
+      pageIndex = 1;
+    } else {
+      pageIndex = n;
+    }
+  }
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].classList.remove("paging-number__item--active");
+  }
+  pages[pageIndex-1].classList.add("paging-number__item--active");
+  showPageArrow(pageIndex);
+}
+
+function showPageArrow(n) {
+  pageIndex =  n;
+  if (n == 1) {
+    arrow_previous.classList.add("paging__arrow--hidden");
+  } else {
+    arrow_previous.classList.remove("paging__arrow--hidden");
+  }
+  if (n == 6) {
+    arrow_next.classList.add("paging__arrow--hidden");
+  } else {
+    arrow_next.classList.remove("paging__arrow--hidden");
+  }
+}
